db0f1720ec635f2083b5c36240f257212ecd4900
1ze <mag140.py@gmail.com>
2025-04-04 23:55:00 +0200

    Create button_press.py

---
+++ projects/projects/push-button/button_press.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+import time
+
+# Configuration des broches
+bouton_pin = 17
+
+# Configuration initiale de la GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(bouton_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+try:
+    print("Attendez que le bouton soit pressé...")
+
+    while True:
+        if GPIO.input(bouton_pin) == GPIO.LOW:
+            print("Bouton pressé!")
+            time.sleep(0.2)  # Débouncing
+            while GPIO.input(bouton_pin) == GPIO.LOW:
+                time.sleep(0.1)  # Attente du relâchement
+        time.sleep(0.1)
+
+except KeyboardInterrupt:
+    pass
+
+finally:
+    GPIO.cleanup()
